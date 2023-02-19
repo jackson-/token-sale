d62295176128e85549cdf5f8823f128a928bd4f9
Devin Jackson <devinjackson@desktop-p4j6gs7.mynetworksettings.com>
2023-02-19 16:28:03 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ERC20 Token Sale
+
+Assignment description
+
+Add a function where users can mint 1000 tokens if they pay 1 ether.
+
+IMPORTANT: your token should have 18 decimal places as is standard in ERC20 tokens
+
+IMPORTANT: your total supply should not exceed 1 million tokens. The sale should close after 1 million tokens have been minted
+
+IMPORTANT: you must have a function to withdraw the ethereum from the contract to your address
---
+++ TokenSale.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20TokenSale is Ownable, ERC20 {
+
+    constructor(uint256 initialSupply) ERC20("TestToken", "TEST") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mintTokens() public payable returns (bool success) {
+        require(msg.value == 1 ether, "Insuffienct funds to mint tokens. Cost is 1 ether.");
+        require(totalSupply() >= 1000000, "Token sale has reached one million tokens. Token sale is over.");
+        _mint(msg.sender, 1000);
+        return true;
+    }
+
+    function withdrawSaleFunds() external onlyOwner returns (bool) {
+        address self = address(this);
+        require(payable(self).balance > 0, "Balance is empty. Nothing to transfer.");
+        payable(msg.sender).transfer(payable(self).balance);
+        return true;
+    }
+
+}
